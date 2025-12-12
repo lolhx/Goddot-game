38b42172c0b48e01def9003e0e4aa3af89397938
zak baruzi <zak.baruzi@gmail.com>
2025-12-12 18:56:45 +0000

    First sword attack attempt

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,22 +1,30 @@
 extends CharacterBody2D
 
+# --- MOVEMENT SETTINGS ---
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 const WALL_JUMP_VELOCITY = Vector2(750, -300)
 const GRAVITY = 800.0
 const WALL_SLIDE_SPEED = 30.0 
-
 var DASH_SPEED = 400.0
 var dashing = false 
 var cand_dash = true
 
+# --- INTERNAL VARIABLES ---
 var direction = 0
 var gundirection = Vector2.ZERO
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-# --- MANA VARIABLES ---
+# --- MANA & SPELL SETTINGS ---
 var current_mana = 0.0
 var mana_cost = 20.0
+
+# --- JUMP SETTINGS ---
+var jumps_left = 2
+
+# --- COMBAT SETTINGS ---
+var is_attacking = false
+var sword_damage = 20 # How much damage the sword does
 
 # --- NODES ---
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
@@ -28,23 +36,28 @@ var mana_cost = 20.0
 @onready var mana_label = $CanvasLayer/ProgressBar/ManaLabel
 @onready var shop_menu = $CanvasLayer/ShopMenu 
 
+# Sword References (Make sure names match exactly in your Scene Tree!)
+@onready var sword_area = $SwordArea 
+@onready var sword_sprite = $SwordArea/Sprite2D
+@onready var sword_collider = $SwordArea/CollisionShape2D
+
 var fireball_path = preload("res://scene/fireballl_attack.tscn")
 
-var jumps_left = 2
-
 func _ready():
+	# Initialize Stats
 	current_mana = Global.max_mana
+	
+	# Hide UI overlays
 	if shop_menu:
 		shop_menu.visible = false
-
-func fire(fire_direction):
-	var fireball = fireball_path.instantiate()
-	fireball.dir = fire_direction
-	fireball.pos = $fireballPosition.global_position
-	get_parent().add_child(fireball)
+	
+	# Initialize Sword (Hidden and Safe)
+	if sword_area:
+		sword_area.monitoring = false
+		sword_sprite.visible = false
 
 func _physics_process(delta):
-	# --- SHOP INPUT ---
+	# --- SHOP / PAUSE LOGIC ---
 	if Input.is_action_just_pressed("shop"): 
 		if shop_menu.visible:
 			shop_menu.visible = false
@@ -56,21 +69,24 @@ func _physics_process(delta):
 	if get_tree().paused:
 		return
 
-	# --- MANA LOGIC ---
+	# --- MANA REGENERATION ---
 	if current_mana < Global.max_mana:
 		current_mana += Global.mana_regen * delta
 		if current_mana > Global.max_mana:
 			current_mana = Global.max_mana
 	
-	# --- UPDATE UI ---
+	# --- UPDATE MANA UI ---
 	if mana_bar:
 		mana_bar.max_value = Global.max_mana
 		mana_bar.value = current_mana
-	
 	if mana_label:
 		mana_label.text = str(int(current_mana)) + " / " + str(int(Global.max_mana))
 
-	# --- FIREBALL LOGIC ---
+	# --- MELEE ATTACK INPUT ---
+	if Input.is_action_just_pressed("attack") and not is_attacking:
+		attack()
+
+	# --- FIREBALL INPUT ---
 	if Input.is_action_just_pressed("fire"):
 		if current_mana >= mana_cost:
 			current_mana -= mana_cost
@@ -85,18 +101,23 @@ func _physics_process(delta):
 
 	# --- MOVEMENT LOGIC ---
 	
-	# [CHANGE 1] Reset jumps to the Global max when hitting the floor
+	# Reset Jumps when on floor
 	if is_on_floor():
 		jumps_left = Global.max_jumps
 
+	# Aiming & Sword Direction
 	if Input.is_action_just_pressed("left"):
 		FireballAim.position.x = -9
+		if sword_area: sword_area.scale.x = -1 # Flip sword to left
 	if Input.is_action_just_pressed("right"):
 		FireballAim.position.x = 9
+		if sword_area: sword_area.scale.x = 1  # Flip sword to right
 		
+	# Gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
+	# Wall Slide & Jump
 	if is_on_wall() and !is_on_floor():
 		if velocity.y > WALL_SLIDE_SPEED:
 			velocity.y = WALL_SLIDE_SPEED
@@ -107,18 +128,18 @@ func _physics_process(delta):
 		else:
 			velocity.x = direction * SPEED
 	
+	# Jumping
 	if Input.is_action_just_pressed("jump"):
 		if is_on_wall():
 			var normal = get_wall_normal()
 			velocity = Vector2(WALL_JUMP_VELOCITY.x * normal.x, WALL_JUMP_VELOCITY.y)
-			
-			# [CHANGE 2] Reset jumps to Global max when wall jumping
-			jumps_left = Global.max_jumps
+			jumps_left = Global.max_jumps # Reset jumps on wall jump
 			
 		elif jumps_left > 0:
 			velocity.y = JUMP_VELOCITY
 			jumps_left -= 1
-		
+	
+	# Dashing
 	if Input.is_action_just_pressed("dash") and cand_dash:
 		dashing = true
 		cand_dash = false
@@ -131,22 +152,27 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
+	# Sprite Flipping
 	if direction > 0:
 		animated_sprite.flip_h = false
 	elif direction < 0:
 		animated_sprite.flip_h = true
 		
-	if dashing == false:
-		if is_on_floor():
-			if direction == 0:
-				animated_sprite.play("Idle")
+	# --- ANIMATION CONTROL ---
+	# We only play movement animations if we are NOT attacking.
+	if not is_attacking:
+		if dashing == false:
+			if is_on_floor():
+				if direction == 0:
+					animated_sprite.play("Idle")
+				else:
+					animated_sprite.play("run")
 			else:
-				animated_sprite.play("run")
+				animated_sprite.play("jump")
 		else:
-			animated_sprite.play("jump")
-	else:
-		animated_sprite.play("Dash")
+			animated_sprite.play("Dash")
 		
+	# Apply Movement
 	if direction:
 		if dashing:
 			velocity.x = direction * DASH_SPEED
@@ -157,6 +183,41 @@ func _physics_process(delta):
 
 	move_and_slide()
 
+# --- COMBAT FUNCTIONS ---
+
+func fire(fire_direction):
+	var fireball = fireball_path.instantiate()
+	fireball.dir = fire_direction
+	fireball.pos = $fireballPosition.global_position
+	get_parent().add_child(fireball)
+
+func attack():
+	is_attacking = true
+	
+	# 1. Enable the Sword
+	sword_sprite.visible = true
+	sword_area.monitoring = true
+	
+	# Optional: Play an attack animation if you made one
+	# animated_sprite.play("attack")
+	
+	# 2. Wait for the swing (0.3 seconds)
+	await get_tree().create_timer(0.3).timeout
+	
+	# 3. Disable the Sword
+	sword_area.monitoring = false
+	sword_sprite.visible = false
+	is_attacking = false
+
+# This function runs when the Sword Hitbox touches something
+func _on_sword_area_body_entered(body):
+	if body.has_method("take_damage"):
+		print("Slash! Hit enemy.")
+		body.take_damage(sword_damage)
+	elif body.has_method("die"):
+		body.die()
+
+# --- TIMERS ---
 func _on_dash_timeout():
 	dashing = false
 
